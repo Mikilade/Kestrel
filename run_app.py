0364ca51729c4fd4435460c4b483381a1cf428f3
Mikilade <mlewis.c.angeles@gmail.com>
2024-10-18 18:11:14 -0600

    backend changes 2

--- run_app.py
+++ run_app.py
@@ -2,17 +2,10 @@ import subprocess
 import os
 from app import create_app
 
-def run_frontend():
-    os.chdir('kestrel-frontend')
-    subprocess.Popen(['npm', 'start'])
-    os.chdir('..')
-
 def run_backend(environ, start_response):
     app = create_app()
     return app(environ, start_response)
 
 if __name__ == '__main__':
-    run_frontend()
-    app = create_app()
     app.run()
 
